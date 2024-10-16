c61ac478d8d56d7b4b0fe9963f8eb840d3f56ee3
Pemba Gurung <gurungpemba.t@gmail.com>
2024-10-15 22:30:56 -0500

    TaskQuack springboot project.

---
+++ task-quack-server-side/src/main/java/edu/stthomas/task_quack_server_side/TaskQuackServerSideApplication.java
@@ -0,0 +1,13 @@
+package edu.stthomas.task_quack_server_side;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskQuackServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskQuackServerSideApplication.class, args);
+	}
+
+}
---
+++ task-quack-server-side/src/test/java/edu/stthomas/task_quack_server_side/TaskQuackServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.stthomas.task_quack_server_side;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskQuackServerSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
