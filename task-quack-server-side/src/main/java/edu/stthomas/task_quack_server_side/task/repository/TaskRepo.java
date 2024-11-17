82109468f7b1f4884b15f738ec1e05bb1b959216
Pemba Gurung <50277580+petse-grg@users.noreply.github.com>
2024-11-17 13:10:28 -0600

    implemented findBylistId repo function with JPQL query to find task using listId

---
+++ task-quack-server-side/src/main/java/edu/stthomas/task_quack_server_side/task/repository/TaskRepo.java
@@ -0,0 +1,17 @@
+package edu.stthomas.task_quack_server_side.task.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import edu.stthomas.task_quack_server_side.task.model.Tasks;
+
+@Repository
+public interface TaskRepo extends JpaRepository<Tasks, Long> {
+    @Query("SELECT t FROM Tasks t WHERE t.list.id = :listId")
+    List<Tasks> findByListId(@Param("listId") Integer listId);
+}
