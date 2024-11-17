3bbbd5620147cc9b603f1605a0d1a851007f571c
Pemba Gurung <50277580+petse-grg@users.noreply.github.com>
2024-11-17 13:02:42 -0600

    replaced with new changes

--- task-quack-server-side/src/main/java/edu/stthomas/task_quack_server_side/list/ListRepo.java
+++
@@ -1,9 +0,0 @@
-package edu.stthomas.task_quack_server_side.list;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ListRepo extends JpaRepository<ListContent, Long> {
-    
-}
