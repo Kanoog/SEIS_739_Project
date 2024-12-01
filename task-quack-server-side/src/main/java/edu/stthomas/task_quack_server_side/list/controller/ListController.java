1218374f82f1124db1a5d9d740874ee97ef61cdb
Pemba Gurung <50277580+petse-grg@users.noreply.github.com>
2024-11-30 20:39:38 -0600

    Fixed cors issue

--- task-quack-server-side/src/main/java/edu/stthomas/task_quack_server_side/list/controller/ListController.java
+++ task-quack-server-side/src/main/java/edu/stthomas/task_quack_server_side/list/controller/ListController.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.stthomas.task_quack_server_side.list.model.Lists;
 import edu.stthomas.task_quack_server_side.list.service.ListService;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController()
 @RequestMapping("/api/lists")
 public class ListController {
